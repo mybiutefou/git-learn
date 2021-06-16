aacabdad5875ce92e9ffbd2505e0d5ebf7662488
biutefou <869119270@qq.com>
2021-06-16 08:17:09 +0800

    first commit

---
+++ src/main/java/com/biu/dao/biu.java
@@ -0,0 +1,11 @@
+package com.biu.dao;
+
+/**
+ * @Author: biutefou
+ * @Date: 2021-06-15 下午 08:39
+ */
+public class biu {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/com/biu/dao/biubiu.java
@@ -0,0 +1,8 @@
+package com.biu.dao;
+
+/**
+ * @Author: biutefou
+ * @Date: 2021-06-16 上午 08:14
+ */
+public class biubiu {
+}
